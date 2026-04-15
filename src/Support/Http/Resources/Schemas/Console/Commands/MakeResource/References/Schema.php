c11fb2116a117a7b8b437565cbdfb77a3735604b
Deven Jahnke <10586846+devenjahnke@users.noreply.github.com>
2026-04-14 22:38:00 -0400

    chore: bump aryeo/tooling-laravel to 0.0.36 (#15)

--- src/Support/Http/Resources/Schemas/Console/Commands/MakeResource/References/Schema.php
+++ src/Support/Http/Resources/Schemas/Console/Commands/MakeResource/References/Schema.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Support\Http\Resources\Schemas\Console\Commands\MakeResource\References;
 
+use Illuminate\Support\Stringable;
 use Tooling\GeneratorCommands\References\GenericClass;
 
-final class Schema extends GenericClass {}
+final class Schema extends GenericClass
+{
+    public Stringable $stubPath {
+        get => str(__DIR__.'/stubs/schema.stub');
+    }
+}
